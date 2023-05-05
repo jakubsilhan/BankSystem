66ae403ae8c913390d2486b76992edbcabd281f5
Jakub Silhan <jakub.silhan@tul.cz>
2023-05-05 21:20:01 +0200

    First working api authentication

--- Backend/src/main/java/backend/controller/AuthenticationController.java
+++ Backend/src/main/java/backend/controller/AuthenticationController.java
@@ -6,39 +6,40 @@ import backend.dto.AuthRequest;
 import backend.services.JsonUserService;
 import backend.services.JwtService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.AccessDeniedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class AuthenticationController {
     @Autowired
     JsonUserService userService;
         
     @Autowired
     private JwtService jwtService;
     
-    @RequestMapping("/")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String hello() throws JsonProcessingException {
+    
+    @PostMapping("/")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public long hello(@RequestHeader(name="Authorization") String token) throws JsonProcessingException {
+        
+        token = token.substring(7);
+        return jwtService.getAccountNumber(token);
 //        User user = userService.getUserByEmail(email);
 //        ObjectMapper objectMapper = new ObjectMapper();
-//        return objectMapper.writeValueAsString(user);      
-            return "pomoc";
+//        return objectMapper.writeValueAsString(user);   
     }    
+    
     
     @PostMapping("/authenticate")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) throws AccessDeniedException{
         User user = userService.getUserByEmail(authRequest.getUsername());
         if (user.getPassword().equals(authRequest.getPassword())){
-            return jwtService.generateToken(authRequest.getUsername());
+            return jwtService.generateToken(user.getUsername(), user.getAccountNumber());
         }
         return user.getPassword()+" != " + authRequest.getPassword();
     }
