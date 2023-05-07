576a8305538003d1fa5022a3f558482c9d63bd4f
Jakub Silhan <jakub.silhan@tul.cz>
2023-05-07 22:04:03 +0200

    Working currency conversion

--- Backend/src/main/java/backend/services/JsonAccountService.java
+++ Backend/src/main/java/backend/services/JsonAccountService.java
@@ -9,15 +9,20 @@ import backend.repositories.AccountRepository;
 import backend.repositories.JsonAccountRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JsonAccountService implements AccountService{
-
+    
+    CurrencyExchangeService exchangeService = new CurrencyExchangeService();
     private List<Account> accounts;
     ObjectMapper objectMapper = new ObjectMapper();
     AccountRepository repository = new JsonAccountRepository();
@@ -41,6 +46,23 @@ public class JsonAccountService implements AccountService{
                 .filter(b -> b.getAbbreviation().equals(payment.getCurrencyAbbreviation()))
                 .findFirst()
                 .orElse(null);
+        if(balance==null || balance.getAmount()<payment.getAmmount()){            
+            try {
+                int exchanged = exchangeService.ConvertToCZK(payment.getCurrencyAbbreviation(), payment.getAmmount());
+                balance = account.getBalances().stream()
+                    .filter(b -> b.getAbbreviation().equals("CZK"))
+                    .findFirst()
+                    .orElse(null);
+                if(balance.getAmount()<exchanged){
+                    throw new IllegalArgumentException("Nedostatek prostredku");
+                }
+                payment.setCurrencyAbbreviation("CZK");
+                payment.setAmmount(exchanged);
+            } catch (IOException ex) {
+                Logger.getLogger(JsonAccountService.class.getName()).log(Level.SEVERE, null, ex);
+                return;
+            }
+        }
         balance.setAmount(balance.getAmount()-payment.getAmmount());
         // Add a new movement for the deposit
         LocalDate date = LocalDate.now();        
@@ -59,6 +81,20 @@ public class JsonAccountService implements AccountService{
                 .filter(b -> b.getAbbreviation().equals(payment.getCurrencyAbbreviation()))
                 .findFirst()
                 .orElse(null);
+        if(balance == null){
+            try {
+                int exchanged = exchangeService.ConvertToCZK(payment.getCurrencyAbbreviation(), payment.getAmmount());
+                balance = account.getBalances().stream()
+                    .filter(b -> b.getAbbreviation().equals("CZK"))
+                    .findFirst()
+                    .orElse(null);
+                payment.setCurrencyAbbreviation("CZK");
+                payment.setAmmount(exchanged);
+            } catch (IOException ex) {
+                Logger.getLogger(JsonAccountService.class.getName()).log(Level.SEVERE, null, ex);
+                return;
+            }
+        }
         balance.setAmount(balance.getAmount()+payment.getAmmount());
         // Add a new movement for the deposit
         LocalDate date = LocalDate.now();        
@@ -90,5 +126,11 @@ public class JsonAccountService implements AccountService{
         }catch(JsonProcessingException e){
             return "Cannot load current movements";
         }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        JsonAccountService jsonService = new JsonAccountService();
+        PaymentDto payment = new PaymentDto("BRL", 1);
+        jsonService.withdraw(12345, payment);
     }
 }
