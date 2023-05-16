fbcef6e4d0e0291780311b6554109ca28a0f21eb
jakubsilhan <100836112+jakubsilhan@users.noreply.github.com>
2023-05-16 09:12:58 +0200

    Working exchange rate update (#11)

--- Backend/src/main/java/backend/BackendApplication.java
+++ Backend/src/main/java/backend/BackendApplication.java
@@ -1,5 +1,13 @@
 package backend;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +18,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 @ComponentScan(basePackages = "backend")
 public class BackendApplication {
 
-    @Scheduled(cron = "0 46 21 * * ?", zone = "Europe/Paris")
-    private void mrdej() {
-        System.out.println("Vole");
+    @Scheduled(cron = "0 35 14 * * ?", zone = "Europe/Paris")
+    private void updateCurrencies() throws MalformedURLException, IOException {
+        InputStream in = new URL("https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt").openStream();
+        Files.copy(in, Paths.get(System.getProperty("user.dir") + File.separator + "data" + File.separator + "currencies.txt"), StandardCopyOption.REPLACE_EXISTING);
     }
     
     public static void main(String[] args) {
